643bdc75f7f119c5a956a323b0f329145d3ae047
Abel DIDOUH <122778382+abel-did@users.noreply.github.com>
2023-05-25 19:42:47 +0200

    buzzer_camera_ligne.ino

---
+++ MSP432P401R/src/buzzer_camera_ligne.ino
@@ -0,0 +1,60 @@
+// Guide Pool Dog
+// Date : 22 / 05 / 2023
+
+#include <Wire.h>
+#include <stdio.h>
+#include <PixyI2C.h>
+
+#define FREQ 1450
+int buzzerPin = 19;
+int arrayFREQ[] = {FREQ};
+int FREQDurations[] = {2};
+
+PixyI2C pixy(0x55); // I2C Address
+
+#define LED RED_LED
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+  
+  pixy.init();
+}
+
+
+void loop() 
+{ 
+  static int i = 0;
+  int j;
+  uint16_t blocks;
+  char buf[32]; 
+  
+  blocks = pixy.getBlocks(1); // Just take the first block
+  
+  if (blocks)
+  {
+    i++;
+    
+    if (i%50==0) // Every 50 Frames
+    {
+      if(pixy.blocks[j].x > 45)
+      {
+          buzzer();
+      }
+      
+    }
+  }  
+}
+
+void buzzer(void)
+{
+    int FREQDuration = 1000/FREQDurations[0];
+    tone(buzzerPin, arrayFREQ[0], FREQDuration);
+
+    int pauseBetweenFREQs = FREQDuration + 50; 
+    delay(pauseBetweenFREQs);
+
+    noTone(buzzerPin);
+}
